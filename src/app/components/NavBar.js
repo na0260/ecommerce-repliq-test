640fb63a786ef94fb4b732e218766f11dd58fbb4
na0260 <nur.201002398@green.ac.bd>
2024-02-07 17:07:53 +0600

    modified

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -17,7 +17,32 @@ const NavBar = () => {
         );
     }, []);
 
+    const navList = (
+        <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+            <Typography
+                as="li"
+                variant="small"
+                color="blue-gray"
+                className="flex items-center gap-x-2 p-1 font-medium"
+            >
 
+                <a href="#" className="flex items-center">
+                    Products
+                </a>
+            </Typography>
+            <Typography
+                as="li"
+                variant="small"
+                color="blue-gray"
+                className="flex items-center gap-x-2 p-1 font-medium"
+            >
+
+                <a href="#" className="flex items-center">
+                    Cart
+                </a>
+            </Typography>
+        </ul>
+    );
 
     return (
         <Navbar className="mx-auto max-w-screen-xl px-4 py-2 lg:px-8 lg:py-4">
@@ -29,11 +54,8 @@ const NavBar = () => {
                 >
                     Repliq Ecommerce
                 </Typography>
-                <div className="hidden lg:block"></div>
+                <div className="hidden lg:block">{navList}</div>
                 <div className="flex items-center gap-x-1">
-                    <Button variant="text" size="sm" className="hidden lg:inline-block">
-                        <span>Create account</span>
-                    </Button>
                     <Button
                         variant="gradient"
                         size="sm"
@@ -82,10 +104,8 @@ const NavBar = () => {
             </div>
             <MobileNav open={openNav}>
                 <div className="container mx-auto">
+                    {navList}
                     <div className="flex items-center gap-x-1">
-                        <Button fullWidth variant="text" size="sm" className="">
-                            <span>Create account</span>
-                        </Button>
                         <Button fullWidth variant="gradient" size="sm" className="">
                             <span>Sign in</span>
                         </Button>
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
 import NavBar from "@/app/components/NavBar";
-import {Typography} from "@/app/utils/materialComponents";
+import Register from "@/app/components/auth/Register";
 
 export default function Home() {
   return (
       <>
         <NavBar/>
-        <Typography className="text-center m-10" variant="h4" color="blue-gray">
-            This is the home page
-        </Typography>
+        <Register/>
       </>
   );
 }
